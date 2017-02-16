a33fb91d74d07fe452c88c9ee9853579a74ed91e
Sachin <sraturi>
2017-02-15 17:06:16 -0800

    first commit,
    added welcome screen. still working on the animation.
    mave main menu with 3 buttons.

---
+++ app/src/main/java/com/example/sachin/mineseeker/MainMenu.java
@@ -0,0 +1,29 @@
+package com.example.sachin.mineseeker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        Intent newGameIntent = new Intent(MainMenu.this,Welcome_Screen.class);
+        OnButtonClick(R.id.button,newGameIntent);
+
+    }
+
+    private void OnButtonClick(int buttonId, final Intent intent) {
+        Button button = (Button) findViewById(buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sachin/mineseeker/Welcome_Screen.java
@@ -0,0 +1,43 @@
+package com.example.sachin.mineseeker;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class Welcome_Screen extends AppCompatActivity {
+
+    ProgressBar loadingCircle;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_creen);
+        ImageView loadingImg = (ImageView)findViewById(R.id.loadingImg);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.animation_welcome);
+
+        loadingImg.setAnimation(animation);
+
+        loadingCircle = (ProgressBar) findViewById(R.id.loadingProgressBar);
+        ObjectAnimator objAnim  = ObjectAnimator.ofInt(loadingCircle,"Loading",0,2000);
+        objAnim.setDuration(3000);
+        objAnim.setInterpolator(new DecelerateInterpolator());
+        objAnim.start();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome_Screen.this,MainMenu.class);
+                startActivity(intent);
+                finish();
+            }
+        },10000);
+    }
+
+}
